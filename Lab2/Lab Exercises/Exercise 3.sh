38ac0a38d2c04eb88f5f3b75cd8f9db60deee301
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2026-07-10 16:10:16 +0600

    Create Exercise 3 script for number classification
    Add a script to print numbers 1 to 20 with even/odd and multiple of 3 status.

--- Lab2/Lab Exercises/Exercise 3.sh
+++ Lab2/Lab Exercises/Exercise 3.sh
@@ -1,1 +1,17 @@
 
+#!/bin/bash
+
+for (( i=1; i<=20; i++ ))
+do
+    if [ $((i % 2)) -eq 0 ]; then
+        output="$i is Even"
+    else
+        output="$i is Odd"
+    fi
+
+    if [ $((i % 3)) -eq 0 ]; then
+        output="$output, Multiple of 3"
+    fi
+
+    echo "$output"
+done

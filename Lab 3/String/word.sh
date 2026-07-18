f20033064be38f9bcb32059669280d1004b27d35
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2026-07-18 12:31:52 +0600

    Update word.sh

--- Lab 3/String/word.sh
+++ Lab 3/String/word.sh
@@ -1,1 +1,43 @@
+name="Bangladesh"
+echo "Country: $name"
 
+echo " Length: ${#name}"
+
+echo "${name:0:4}"
+echo "${name:4}"
+
+first="Computer"
+second="Science"
+full="$first $second"
+echo "$full"
+
+sentence="I like Java, Java is powerfull"
+echo "${sentence/Java/Python}"
+echo "${sentence//Java/Python}"
+
+str1="abc"
+str2="xyz"
+if [ "$str1" = "$str2" ]; then
+	echo "Strings are equal"
+else 
+	echo "Strings are not euqal"
+fi
+
+word="Hello"
+echo "${word^^}"
+echo "${word,,}"
+
+echo "Enter a word:"
+read word
+reverse=""
+length=${#word}
+
+for(( i=length-1; i>=0; i--)); do
+	reverse="$reverse${word:i:1}"
+done
+
+if [ "$word" = "$reverse" ]; then
+	echo "$word is a palindrome"
+else
+	echo "$word is not a palindrome"
+fi
